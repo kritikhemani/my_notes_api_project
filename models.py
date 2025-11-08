02d6c861e64031779a5312e52629267961f33c88
Kriti Khemani <kritikhemani04@gmail.com>
2025-11-08 11:01:47 +0530

    feat: Add a function

--- models.py
+++ models.py
@@ -1,6 +1,12 @@
 from pydantic import BaseModel, Field
+from typing import Optional
 
 
 class NewNote(BaseModel):
     title: str = Field(..., min_length=1)
     content: str = Field(..., min_length=1)
+    
+class NoteUpdate(BaseModel):
+    title: Optional[str] = Field(None, min_length=1)
+    content: Optional[str] = Field(None, min_length=1)
+    
