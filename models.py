aa9640453dac5a742ab6660d966155f22abc47d8
Kriti Khemani <kritikhemani04@gmail.com>
2025-11-08 11:06:10 +0530

    feat: Adding function

--- models.py
+++ models.py
@@ -10,3 +10,9 @@ class NoteUpdate(BaseModel):
     title: Optional[str] = Field(None, min_length=1)
     content: Optional[str] = Field(None, min_length=1)
     
+class NoteResponse(BaseModel):
+    id: int
+    title: str
+    content: str
+
+    
