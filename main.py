bc02ed42c9d1a96c3448a6ecfd9c0085e9ecd7d6
Kriti Khemani <kritikhemani04@gmail.com>
2025-11-09 11:15:54 +0530

    feat: Add create_note function to main.py

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 import database
+from models import NewNote, NoteUpdate, NoteResponse
 
 
 app = FastAPI(title="Notes API")
 
 def get_db():
     return database
+
+@app.post("/notes/", response_model=NoteResponse)
+def create_note(note: NewNote, db=Depends(get_db)):
+    return db.create(note)
