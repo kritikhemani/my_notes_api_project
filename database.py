416f84de367dbcd7979656c50111c6a2297e8de3
Kriti Khemani <kritikhemani04@gmail.com>
2025-11-10 10:59:43 +0530

    feat: Adding function to database.py

--- database.py
+++ database.py
@@ -3,3 +3,14 @@ note_id_counter = 1
 
 def get_all(skip=0, limit=10):
     return notes[skip: skip + limit]
+
+def create(note_data):
+    global note_id_counter
+    note = {
+        "id": note_id_counter,
+        "title": note_data.title,
+        "content": note_data.content
+    }
+    notes.append(note)
+    note_id_counter += 1
+    return note
