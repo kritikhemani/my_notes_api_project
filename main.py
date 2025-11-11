c1f43ceb17d45309065bebb149c72bfd957aff40
Kriti Khemani <kritikhemani04@gmail.com>
2025-11-11 12:11:00 +0530

    feat: Add read_notes function to main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 from typing import List
 import database
 from models import NewNote, NoteUpdate, NoteResponse
@@ -16,3 +16,10 @@ def create_note(note: NewNote, db=Depends(get_db)):
 @app.get("/notes/", response_model=List[NoteResponse])
 def read_notes(skip: int = 0, limit: int = 10, db=Depends(get_db)):
     return db.get_all(skip, limit)
+
+@app.get("/notes/{note_id}", response_model=NoteResponse)
+def read_notes(note_id: int, db=Depends(get_db)):
+    note = db.get_one(note_id)
+    if note is None:
+        raise HTTPException(status_code=404, detail="Note not found")
+    return note
